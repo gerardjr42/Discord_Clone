801d213d06354f946f624c927c334372928b582f
gerardjr42 <ggarciajr12@gmail.com>
2024-11-02 02:23:56 -0400

    Add pending friends icons 1. Create icons for accept and reject 2. Add colors to the buttons

--- src/app/(dashboard)/_components/friends-list.tsx
+++ src/app/(dashboard)/_components/friends-list.tsx
@@ -1,7 +1,7 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { useQuery } from "convex/react";
-import { MessageCircleIcon, XIcon } from "lucide-react";
+import { CheckIcon, MessageCircleIcon, XIcon } from "lucide-react";
 import { api } from "../../../../convex/_generated/api";
 
 const useTestUsers = () => {
@@ -18,7 +18,24 @@ export function PendingFriendsList() {
     <div className="flex flex-col divide-y">
       <h2 className="text-sm text-muted-foreground p-2.5">Pending Friends</h2>
       {users.map((user, index) => (
-        <FriendItem key={index} username={user.username} image={user.image} />
+        <FriendItem key={index} username={user.username} image={user.image}>
+          <Button
+            className="rounded-full bg-green-100"
+            variant="outline"
+            size="icon"
+          >
+            <CheckIcon />
+            <span className="sr-only">Accept</span>
+          </Button>
+          <Button
+            className="rounded-full bg-red-100"
+            variant="outline"
+            size="icon"
+          >
+            <XIcon />
+            <span className="sr-only">Reject</span>
+          </Button>
+        </FriendItem>
       ))}
     </div>
   );
@@ -35,7 +52,11 @@ export function AcceptedFriendsList() {
             <MessageCircleIcon />
             <span className="sr-only">Start DM</span>
           </Button>
-          <Button className="rounded-full" variant="outline" size="icon">
+          <Button
+            className="rounded-full bg-red-100"
+            variant="outline"
+            size="icon"
+          >
             <XIcon />
             <span className="sr-only">Remove</span>
           </Button>
