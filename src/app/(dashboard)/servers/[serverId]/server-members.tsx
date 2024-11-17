6ff551cee95157e0c0aeaa9b2f55a759e79df72b
gerardjr42 <ggarciajr12@gmail.com>
2024-11-16 22:26:27 -0500

    feat: Add server invite creation and enhance UI components
    - Added `create-invite.tsx` to implement server invite functionality, enabling users to generate and manage invites for server access.
    - Enhanced server member management in `server-members.tsx` with improved handling and validation of server members.

--- src/app/(dashboard)/servers/[serverId]/server-members.tsx
+++ src/app/(dashboard)/servers/[serverId]/server-members.tsx
@@ -2,6 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useQuery } from "convex/react";
 import { api } from "../../../../../convex/_generated/api";
 import { Id } from "../../../../../convex/_generated/dataModel";
+import { CreateInvite } from "./create-invite";
 
 export function ServerMembers({ id }: { id: Id<"servers"> }) {
   const members = useQuery(api.functions.server.members, { id });
@@ -17,6 +18,7 @@ export function ServerMembers({ id }: { id: Id<"servers"> }) {
           <p className="text-sm font-medium">{member.username}</p>
         </div>
       ))}
+      <CreateInvite serverId={id} />
     </div>
   );
 }
